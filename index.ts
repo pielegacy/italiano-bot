c66494874f5f3471be797eb23d5102592b0d8688
Alex Billson <strikingmongoose@hotmail.com>
2017-01-18 23:24:41 +1100

    - Now posts pizza

--- index.ts
+++ index.ts
@@ -6,16 +6,18 @@ client.on('ready', () => {
     console.log("It's a me, Andrew! Call me by typing Italiano");
     // client.channels.get("name", "reddit-free-zone");
 });
-
 client.on('message', message => {
     if (message.content.startsWith("Italiano")) {
+        // TODO : Make this not fucking trash
         let number = Math.random();
         if (number < .3)
             message.reply("Mamma mia!");
         else if (number < .5)
             message.reply("Wowee, I love Anime and Pizza!");
-        else
+        else if (number < .7)
             message.reply("Yahoo! Italy!");
+        else
+            message.reply("https://barbaracartategui.files.wordpress.com/2010/11/pizza-pepperoni.jpg");
     }
 });
 // Check for token.txt
