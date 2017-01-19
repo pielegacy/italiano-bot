264d9ebd63e9907a02ccfdc17019274259d74f6d
Alex Billson <strikingmongoose@hotmail.com>
2017-01-19 16:12:11 +1100

    Italiano remembers things and also processes shit better

--- index.ts
+++ index.ts
@@ -6,18 +6,56 @@ client.on('ready', () => {
     console.log("It's a me, Andrew! Call me by typing Italiano");
     // client.channels.get("name", "reddit-free-zone");
 });
+const sayings: Array<string> = [
+    "Mamma mia!",
+    "Wowee, I love Anime and Pizza!",
+    "Yahoo! Italy!",
+    "https://barbaracartategui.files.wordpress.com/2010/11/pizza-pepperoni.jpg",
+    "http://www.ezilon.com/maps/images/europe/Italian-road-map.gif",
+    "Videogames my man!",
+    "Weed my dude!",
+    "You found Dark Souls hard? Well gee get good",
+    "Metal music and Vaporwave, that's the stuff",
+    "http://4chan.org/ck is where it's at",
+    ":pizza:"
+];
+const ThoughtsRead = (): Array<string> => {
+    let thoughtString = fs.readFileSync("thoughts.json");
+    let thoughts = thoughtString == undefined ? ([] as Array<string>) : JSON.parse(thoughtString);
+    return thoughts;
+}
+const ThoughtsSave = (thoughts: Array<string>) => {
+    fs.writeFileSync("thoughts.json", JSON.stringify(thoughts));
+}
 client.on('message', message => {
     if (message.content.startsWith("Italiano")) {
-        // TODO : Make this not fucking trash
-        let number = Math.random();
-        if (number < .3)
-            message.reply("Mamma mia!");
-        else if (number < .5)
-            message.reply("Wowee, I love Anime and Pizza!");
-        else if (number < .7)
-            message.reply("Yahoo! Italy!");
-        else
-            message.reply("https://barbaracartategui.files.wordpress.com/2010/11/pizza-pepperoni.jpg");
+        if ((message.content as string).search(" remember ") != -1) {
+            let thought = (message.content as string).split(" remember ")[1];
+            if (thought != "") {
+                let thoughts = ThoughtsRead();
+                thoughts.push(thought);
+                console.log(thoughts);
+                ThoughtsSave(thoughts);
+                message.reply("I shall remember " + thought + " for you");
+            }
+            else
+                message.reply("What do I need to remember?");
+        }
+        else if ((message.content as string).search("what do you know") != -1) {
+            let thoughts = ThoughtsRead();
+            if (thoughts.length > 0) {
+                let index = Math.floor(Math.random() * (thoughts.length));
+                let thought = thoughts[index];
+                message.reply("Well someone told me " + thought);
+            }
+            else
+                message.reply("I don't know anything, tell me to remember something");
+        }
+        else {
+            let index = Math.floor(Math.random() * (sayings.length));
+            let response = sayings[index];
+            message.reply(response);
+        }
     }
 });
 // Check for token.txt
